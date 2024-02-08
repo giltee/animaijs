d8d405164df31f4bbb5838132cbfbcca463c7fe5
Gilbert Comeau <gilbert.a.comeau@gmail.com>
2024-02-08 13:57:31 -0800

    fixed slidedown bug

--- animaijs.ts
+++ animaijs.ts
@@ -10,28 +10,31 @@ export default class Animaijs {
     private el: HTMLElement;
     private originalHeight: number;
 
-    constructor(el: HTMLElement) {
+    constructor(el: HTMLElement | any) {
+        // console.log(el)
         this.el = el;
-        console.log(this.el);
         this.clone = this.el.cloneNode(true);
-        this.clone.style.position = 'absolute';
-        this.clone.style.top = '10000px';
-        this.clone.style.right = '1000px';
-        const compStyle = window.getComputedStyle(this.el);
+        Object.assign(this.clone.style, {
+            display: 'block',
+            visibility: 'hidden'
+        });
+        document.body.append(this.clone);
+        const compStyle = window.getComputedStyle(this.clone);
         this.originalHeight = parseInt(compStyle.height.replace('px', '')) || 0;
+        this.clone.remove();
     }
 
     /**
      * 
-     * @param animation The animation to execute 
-     * @param time      The time for the animation to complete
-     * @param callback  Any clean up required, default empty function
+     * @param { Function } animation The animation to execute 
+     * @param { int } time      The time for the animation to complete
+     * @param { Function } callback  Any clean up required, default empty function
      */
-    private async __animate(animation, time: number, callback = () => {}){
+    private async __animate(animation: (progress: any) => {}, time: number, callback = () => { }) {
 
         const startTime = performance.now();
 
-        while(performance.now() < time + startTime){
+        while (performance.now() < time + startTime) {
             const progress = (performance.now() - startTime) / time;
             // the animation we are processing
             animation(progress);
@@ -42,6 +45,8 @@ export default class Animaijs {
         callback();
     }
 
+
+
     /** 
      * Determines if the element is visible in the dom or not
      * @return {  boolean }  
@@ -49,48 +54,49 @@ export default class Animaijs {
     private __isHidden(): boolean {
         const style = window.getComputedStyle(this.el);
         return (
-            style.display === 'none' 
-            || style.display === 'hidden' 
-            || style.height === '0' 
+            style.display === 'none'
+            || style.display === 'hidden'
+            || style.height === '0'
             || style.height === '0px'
             || style.opacity === '0'
             || style.visibility === 'hidden'
         );
     }
-    
+
     /**
      * 
      * @param {int} time            time per bounce 
      * @param {object} options      options for bounce animation
-     * @param {int} bounces         The number of bounces the animation will execute, default 3
-     * @param {int} bounceHeight    The starting pixel height on the bounce, the subsequent bounces are reduced by 50%;
+     * @param {int} options.bounces         The number of bounces the animation will execute, default 3
+     * @param {int} options.bounceHeight    The starting pixel height on the bounce, the subsequent bounces are reduced by 50%;
      */
     public async bounce(time: number = 150, options = {
         bounces: 3,
         bounceHeight: 10,
-    }){
+    }) {
 
-        const setTranslate = (pixel: number) => { 
+        const setTranslate = (pixel: number) => {
             this.el.style.transform = `translateY(${pixel}px)`;
         };
 
         let startTime = performance.now();
+        const { bounceHeight } = options;
 
-        for(let i = 0; i < options.bounces; i++){
+        let postive = bounceHeight; // the bounce in pixels
+        let negative = -bounceHeight; // the down bounce in pixels
 
-            let postive = 10;
-            let negative = -10;
+        for (let i = 0; i < options.bounces; i++) {
 
-            const pixel = i % 2 === 0 ? 10 : -10;
+            const pixel = i % 2 === 0 ? postive : negative;
 
-            while(performance.now() < time + startTime){
+            while (performance.now() < time + startTime) {
                 const progress = (performance.now() - startTime) / time;
-                
+
                 setTranslate(parseInt(`${pixel * progress}`))
                 // wait for the next frame
                 await new Promise(requestAnimationFrame);
             }
-    
+
             startTime = performance.now();
 
             postive /= 2;
@@ -98,55 +104,121 @@ export default class Animaijs {
 
         }
 
+        Object.assign(this.el.style, { transform: `translateY(${0}px)` })
+
+        return this;
+
     }
 
     /**
      * 
      * @param {int} time time for animation to complete in milliseconds 
+     * @returns {this}
      */
-    public async fadeIn(time = 500){
+    public fadeIn(time = 500) {
         // only fade in if the element is not visible in the DOM
-        if(this.__isHidden()){
+        if (this.__isHidden()) {
 
             // set the initial state css and then start the animation
             Object.assign(this.el.style, { overflow: 'hidden', opacity: 0, display: 'block', visibility: 'initial' });
-            const animation = (progress) =>  this.el.style.opacity = `${progress}`;
+            const animation = (progress: number | string) => this.el.style.opacity = `${progress}`;
             this.__animate(animation, time, () => this.el.style.opacity = '1');
 
         }
+
+        return this;
     }
 
     /**
      * 
      * @param {int} time time for animation to complete in milliseconds 
+     * @returns {this}
      */
-    public async fadeOut(time = 500){
-        
-        if(!this.__isHidden()){
-            Object.assign(this.el.style, { overflow: 'hidden', opacity: 1});
-            const animation = (progress) => this.el.style.opacity = `${1 - progress}`;
-            this.__animate(animation, time, () => this.el.style.opacity = '0');
-            Object.assign(this.el.style, { display: 'none'});
+    public fadeOut(time = 500) {
+
+        if (!this.__isHidden()) {
+            Object.assign(this.el.style, { overflow: 'hidden', opacity: 1 });
+            const animation = (progress: number) => this.el.style.opacity = `${1 - progress}`;
+            this.__animate(animation, time, () => Object.assign(this.el.style, { display: 'none', opacity: 0 }));
+
         }
+
+        return this;
+    }
+
+    /**
+     * 
+     * @param {int} time time for animation to complete in milliseconds 
+     * @returns {this}
+     */
+    public fadeToggle(time = 500) {
+        if (this.__isHidden()) {
+            this.fadeIn(time);
+        } else {
+            this.fadeOut(time);
+        }
+        return this;
     }
 
     /**
      * 
      * @param {int} time time for animation to complete in milliseconds 
      */
-    async grow(time = 500){
+    public grow(time = 500) {
 
-        Object.assign(this.el.style, { transform: `scale3d(0)`, opacity: 1, display: 'block'});
-        const animation = (progress) => this.el.style.transform = `scale3d(${progress},${progress},${progress})`;
+        Object.assign(this.el.style, { transform: `scale3d(0)`, opacity: 1, display: 'block' });
+        const animation = (progress: number | string) => this.el.style.transform = `scale3d(${progress},${progress},${progress})`;
         this.__animate(animation, time, () => this.el.style.transform = 'scale3d(1,1,1)');
 
+        return this;
+
+    }
+
+    /**
+     * 
+     * @param { function }callback  The function you would like to execute when the intersection is met
+     * @param { object }options   The optons of the intersection observer 
+     * @param { boolean | null} options.root 
+     * @param { string } options.rootMargin 
+     * @param { float } options.threshold
+     * @param {object}unobserverAfter 
+     */
+    public observeIntersection(callback: Function, options: IntersectionObserverInit = {
+        root: null,
+        rootMargin: '0px',
+        threshold: .1
+    }, unobserverAfter: number = 0) {
+        let observer: IntersectionObserver;
+        let observeCount = 0;
+        const observerCallback: IntersectionObserverCallback = (entries) => {
+
+            for (const entry of entries) {
+
+                observeCount++;
+                if (entry.isIntersecting) {
+
+                    callback();
+                    if (
+                        unobserverAfter > 0
+                        && observeCount >= unobserverAfter
+                    ) {
+                        observer.unobserve(this.el);
+                    }
+
+                }
+            }
+
+        };
+
+        observer = new IntersectionObserver(observerCallback, options);
+        observer.observe(this.el);
     }
 
     /**
      * 
      * @param {string} behavior One of smooth, instant, auto. Default: smooth
      */
-    scroll(behavior = 'smooth' ) {
+    public scroll(behavior = 'smooth') {
         const x = this.el.offsetLeft;
         const y = this.el.offsetTop;
         window.scroll({
@@ -154,77 +226,63 @@ export default class Animaijs {
             left: x,
             top: y
         });
+
+        return this;
     }
 
     /**
-     * Smooth Scrolls to the element with an option to control the time period. 
      * 
-     * @param {number} time The number of milliseconds to scroll to the element. Default 500 ms;
+     * @param {string} behavior One of smooth, instant, auto. Default: smooth
      */
-    async scrollTimed(time = 500) {
+    public shrink(time = 500) {
 
-        const scrollHeight = window.scrollY + this.el.offsetTop;
-
-        const animation = (progress) => {
-            const height = scrollHeight * progress;
-            window.scrollTo(0, height);
-        };
-        this.__animate(animation, time, () => window.scrollTo(0, scrollHeight));
-        
-    }
-
-    public shrink(time = 500){
-
-        Object.assign(this.el.style, { transform: `scale3d(100)`, opacity: 1, display: 'block'});
-        const animation = (progress) => { this.el.style.transform = `scale3d(${1 - progress},${1 - progress},${1 - progress})`};
+        Object.assign(this.el.style, { transform: `scale3d(100)`, opacity: 1, display: 'block' });
+        const animation = (progress: number) => this.el.style.transform = `scale3d(${1 - progress},${1 - progress},${1 - progress})`;
         this.__animate(animation, time, () => this.el.style.transform = `scale3d(0,0,0)`);
 
+        return this;
     }
 
 
-     /**
-     * 
-     * @param {string} time The time in ms for the slide down animation to complete
-     * 
-     */
+    /**
+    * 
+    * @param {string} time The time in ms for the slide down animation to complete
+    * 
+    */
     public slideDown(time = 500) {
+
 
         // only perform on hidden elements 
         if (this.__isHidden()) {
 
-            // calculate the clone height
             // set the element css for the animation
             Object.assign(this.el.style, { overflow: 'hidden', height: 0, display: 'block' });
-            const animation = (progress) => {
-                // get/set the current pixel height
-                const pixels = parseInt(`${progress * this.originalHeight}`);
-                this.el.style.height = `${pixels}px`;
-            }
+            const animation = (progress: number) => this.el.style.height = `${parseInt(`${progress * this.originalHeight}`)}px`;
+
             this.__animate(animation, time, () => this.el.style.height = `${this.originalHeight}px`);
         }
+
+        return this;
     }
-    
-     /**
-     * 
-     * @param {string} time The time in ms for the slide up animation to complete
-     * 
-     */
+
+    /**
+    * 
+    * @param {string} time The time in ms for the slide up animation to complete
+    * 
+    */
     public async slideUp(time = 500) {
 
-        const style =  window.getComputedStyle(this.el);
+        const style = window.getComputedStyle(this.el);
         const startTime = performance.now();
 
         Object.assign(this.el.style, { overflow: 'hidden' });
         const offsetHeight = parseInt(style.height.replace('px', '')) || 0;
-        
-        const animation = (progress) => {
-            // get/set the current pixel height
-            const pixels = parseInt(`${progress * offsetHeight}`);
-            this.el.style.height = `${offsetHeight - pixels}px`;
-        }
+
+        const animation = (progress: number) => this.el.style.height = `${offsetHeight - parseInt(`${progress * offsetHeight}`)}px`;
 
         this.__animate(animation, time, () => this.el.style.height = '0');
-        
+
+        return this;
     }
 
     /**
@@ -232,13 +290,36 @@ export default class Animaijs {
      * @param {string} time The time in ms for the slide up animation to complete
      * 
      */
-    public slideToggle(time = 500){
+    public slideToggle(time = 500) {
 
         if (this.__isHidden()) {
             this.slideDown(time);
         } else {
             this.slideUp(time);
         }
+        return this;
+    }
+
+
+    /**
+     * 
+     * @param { string } message    The message being typed to the textContent of the element
+     * @param { int } time          The time for each key stoke to execute/
+     */
+    public typeMessage(message: string, time = 100){
+
+        let index = 0;
+        let textContent = "";
+
+        let interval: any = null;
+        interval = setInterval(() => {
+
+            if(textContent.length >= message.length){
+                clearInterval(interval)
+            }
+            this.el.textContent += message.charAt(index++);
+          
+        }, time);
 
     }
 
